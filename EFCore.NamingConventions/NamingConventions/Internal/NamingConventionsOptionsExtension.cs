e048988ab91e389b4d7522158e095ed6961e1358
Shay Rojansky <roji@roji.org>
2020-05-09 10:34:47 +0200

    Take culture into account in ExtensionInfo
    Part of #19

--- EFCore.NamingConventions/NamingConventions/Internal/NamingConventionsOptionsExtension.cs
+++ EFCore.NamingConventions/NamingConventions/Internal/NamingConventionsOptionsExtension.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
@@ -75,19 +77,54 @@ namespace EFCore.NamingConventions.Internal
             public override bool IsDatabaseProvider => false;
 
             public override string LogFragment
-                => _logFragment ??= Extension._namingConvention switch
+            {
+                get
                 {
-                    NamingConvention.SnakeCase => "using snake-case naming ",
-                    NamingConvention.LowerCase => "using lower case naming",
-                    NamingConvention.UpperCase => "using upper case naming",
-                    _ => ""
-                };
+                    if (_logFragment == null)
+                    {
+                        var builder = new StringBuilder();
 
-            public override long GetServiceProviderHashCode() => Extension._namingConvention.GetHashCode();
+                        builder.Append(Extension._namingConvention switch
+                        {
+                            NamingConvention.SnakeCase => "using snake-case naming ",
+                            NamingConvention.LowerCase => "using lower case naming",
+                            NamingConvention.UpperCase => "using upper case naming",
+                            _ => throw new NotImplementedException("Unhandled enum value: " +
+                                                                   Extension._namingConvention)
+                        });
+
+                        if (Extension._culture is null)
+                        {
+                            builder
+                                .Append(" (culture=")
+                                .Append(Extension._culture)
+                                .Append(")");
+                        }
+
+                        _logFragment = builder.ToString();
+                    }
+
+                    return _logFragment;
+                }
+            }
+
+            public override long GetServiceProviderHashCode()
+            {
+                var hashCode = Extension._namingConvention.GetHashCode();
+                hashCode = (hashCode * 3) ^ (Extension._culture?.GetHashCode() ?? 0);
+                return hashCode;
+            }
 
             public override void PopulateDebugInfo(IDictionary<string, string> debugInfo)
-                => debugInfo["Naming:UseNamingConvention"]
+            {
+                debugInfo["Naming:UseNamingConvention"]
                     = Extension._namingConvention.GetHashCode().ToString(CultureInfo.InvariantCulture);
+                if (Extension._culture != null)
+                {
+                    debugInfo["Naming:Culture"]
+                        = Extension._culture.GetHashCode().ToString(CultureInfo.InvariantCulture);
+                }
+            }
         }
     }
 }
